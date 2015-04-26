becfde5a8fb8f784a1ae8a1373f75cef0f5abffb
jonom137 <jonomervis@gmail.com>
2015-04-26 19:33:17 +0200

    Programming Assignment 2 final

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,20 @@
 ## this cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Create an initital blank matrix
         m = matrix
+        ## Sets a new source matrix passed to the function
         set <- function (y = matrix) {
                 x <<- y
                 m <<- NULL
         }
+        ## Provides functionality to retrieve the current source matrix
         get <- function() x
+        ## Sets the matrix inverse into cache
         setinverse <- function(inverse) m <<- inverse
+        ## Provides functionality to retreive the matrix inverse
         getinverse <- function() m
+        ## Provides the list of this functions subfunctions
         list(set = set, get = get, 
              getinverse = getinverse,
              setinverse = setinverse)
@@ -27,13 +33,18 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Returns a matrix that is the inverse of 'x'
         m <- x$getinverse()
+        ## Checks to see if the retrieved matrix is not blank and prints
+        ## it if this is the case 
         if(!is.null(m)) {
                 message("getting chached data")
                 return (m)
         }
+        ## Otherwise the source matrix is retrieved, the inverse is caluclated
+        ## and then cached.
         data <- x$get()
         m <- solve(data)
         x$setinverse(m)
+        ## Finally it is printed to screen.
         m
           
 }
